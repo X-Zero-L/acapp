7238c1879c6116bd7d44883a8f2fcdbc9e6d390e
X-Zero-L <1025799490@qq.com>
2022-12-11 20:57:02 +0800

    bug fix

--- acapp/asgi.py
+++ acapp/asgi.py
@@ -13,13 +13,14 @@ import django
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'acapp.settings')
 django.setup()
 
+
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 from game.routing import websocket_urlpatterns
 
 from channels.layers import get_channel_layer
-channel_layer = get_channel_layer # 实现在channels外面调用channels的内部函数
+channel_layer = get_channel_layer() # 实现在channels外面调用channels的内部函数
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
